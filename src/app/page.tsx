9ee21815821c463a618dda1849c5ebe8653591f9
bmthd <jougennotuki67@gmail.com>
2024-11-15 23:05:10 +0900

    feat:tableサンプル追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Card, Container, Link } from "@yamada-ui/react";
 export default function Home() {
   return (
     <Card as={Container} gap="sm">
+      <Link href="/table">Table</Link>
       <Link href="/about">About</Link>
       <Link href="/form/server">Form</Link>
       <Link href="/form/client">Form (Client)</Link>
