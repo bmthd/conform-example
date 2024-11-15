e64f63f511868494abd8ff936d2a0f8317f34d90
bmthd <jougennotuki67@gmail.com>
2024-11-15 23:07:17 +0900

    fix

--- src/ui/form/controled.tsx
+++ src/ui/form/controled.tsx
@@ -23,21 +23,8 @@ export const DatePickerField: FC<FieldProps<Date> & ComponentProps<typeof DatePi
       labelProps={{ id, htmlFor: inputProps.id }}
       {...{ helperMessage, errorMessage, label, isRequired, "aria-invalid": ariaInvalid }}
     >
-      <DatePicker {...formProps} {...inputProps} aria-labelledby={id} />
+      {/* @ts-expect-error */}
+      <DatePicker {...formProps} {...inputProps} />
     </FormControl>
-  );
-};
-
-import { ReactNode, useCallback, useState } from "react";
-
-const Checkbox: FC<{ children: FC<{ isChecked: boolean }> | ReactNode }> = ({ children }) => {
-  const id = useId();
-  const [isChecked, setChecked] = useState(false);
-  const handleChange = useCallback(() => setChecked((prev) => !prev), [isChecked]);
-  return (
-    <label htmlFor={id}>
-      <input id={id} type="checkbox" checked={isChecked} onChange={handleChange} />
-      {typeof children === "function" ? children({ isChecked }) : children}
-    </label>
   );
 };
